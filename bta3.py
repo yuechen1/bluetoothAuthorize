7a5968dcaaa9d5293b2cf4f11afcccaa3591ed46
LucidThought <alata.it@gmail.com>
2016-11-19 20:23:55 -0700

    comments

--- bta3.py
+++ bta3.py
@@ -83,7 +83,7 @@ class MainWindow(tk.Frame): # MainWindow class defines the contents and behavior
                     i=i+1
             if(nearbyNum == self.authList.size()):
                 self.authLabel['text']="Authorized"
-                break
+                break # This break is only for testing purposes. If the loop ever gets here then we have this function working
 #               self.authLabel['color']
             
 
